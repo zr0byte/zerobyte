b8d37661441114500117bcfa5fd2620320fcbb7c
Sidhanth Pandey <sidhanthpandey@192.168.1.5>
2024-10-27 04:40:35 +0530

    Fix mobile view for docs page

--- src/components/Docs.jsx
+++ src/components/Docs.jsx
@@ -25,9 +25,9 @@ const Docs = () => {
     return (
         <div className='dark:bg-black bg-white w-full flex flex-col min-h-screen'>
             <Header position={"top"}/>
-            <div className='mx-60 md:mx-28 flex justify-center items-center'>
+            <div className='lg:mx-60 md:mx-28 mx-4 flex justify-center items-center'>
                 {/* <h1 className='text-black dark:text-white mt-auto text-6xl font-bold'>Docs</h1> */}
-                <div className='w-[97vw] mt-10'>
+                <div className='w-full mt-10'>
                     <DocsPageLayout />
                 </div>
             </div>
